2ecee26cfd9d0de4aee9a5205c9befd739d6e2f4
Marcin Kawa <lerukcja2002@gmail.com>
2024-06-01 14:38:26 +0200

    update colors file

---
+++ lib/design/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blue = Color(0xFF51B0D7);
+  static const Color primaryColor = Color(0xFFEEEEEE);
+
+  // TicTacToe
+  static const Color TTTprimaryColor = Color(0xFFff4b4b);
+  static const Color TTTsecondaryColor = Color(0xFFffca27);
+  static const Color TTTaccentColor = Color(0xFF4169e8);
+}
