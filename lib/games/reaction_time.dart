f9b81ddfdea7f21a413991a1fbbac2780112a581
Marcin Kawa <lerukcja2002@gmail.com>
2024-06-01 19:27:00 +0200

    Reaction Time Game

--- lib/games/reaction_time.dart
+++ lib/games/reaction_time.dart
@@ -1,34 +1,113 @@
 import 'package:flutter/material.dart';
-import 'package:mobi_games/design/colors.dart';
-import 'package:mobi_games/pages/main_menu.dart';
 
-class ReactionTime extends StatelessWidget {
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class ReactionTime extends StatefulWidget {
   const ReactionTime({super.key});
 
   @override
+  _ReactionTimeState createState() => _ReactionTimeState();
+}
+
+class _ReactionTimeState extends State<ReactionTime> {
+  List<Color> lightColors = List<Color>.filled(5, Colors.red);
+  bool allGreen = false;
+  bool reactionStarted = false;
+  Timer? reactionTimer;
+  int reactionTime = 0;
+
+  void startGame() {
+    setState(() {
+      allGreen = false;
+      reactionStarted = false;
+      reactionTime = 0;
+      lightColors = List<Color>.filled(5, Colors.red);
+    });
+
+    Future.delayed(Duration(seconds: 1), () {
+      changeLights(0);
+    });
+  }
+
+  void changeLights(int index) {
+    if (index < lightColors.length) {
+      setState(() {
+        lightColors[index] = Colors.green;
+      });
+
+      Future.delayed(Duration(seconds: 1), () {
+        changeLights(index + 1);
+      });
+    } else {
+      setState(() {
+        allGreen = true;
+        startReactionTimer();
+      });
+    }
+  }
+
+  void startReactionTimer() {
+    setState(() {
+      reactionStarted = true;
+    });
+
+    reactionTimer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+      setState(() {
+        reactionTime += 10;
+      });
+    });
+  }
+
+  void stopReactionTimer() {
+    if (reactionTimer != null) {
+      reactionTimer!.cancel();
+    }
+    setState(() {
+      reactionStarted = false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Reaction Time'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => const MainMenu()),
-            );
-          },
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Reaction Time Game'),
         ),
-      ),
-      body: Container(
-        color: AppColors.primaryColor,
-        child: const Center(
-          child: Text(
-            'This is Reaction Time',
-            style: TextStyle(
-              fontSize: 24,
-              color: AppColors.blue,
-            ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: lightColors.map((color) {
+                  return Container(
+                    margin: EdgeInsets.all(8.0),
+                    width: 50,
+                    height: 50,
+                    color: color,
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 50),
+              Text(
+                reactionStarted ? '${reactionTime / 1000.0}s' : 'Press Start',
+                style: TextStyle(fontSize: 48),
+              ),
+              SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: allGreen ? stopReactionTimer : null,
+                child: Text('Stop'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: startGame,
+                child: Text('Start'),
+              ),
+            ],
           ),
         ),
       ),
